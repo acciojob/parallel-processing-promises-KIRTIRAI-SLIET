4c9c16d86a4e728df436a69fe267e57d02768cdc
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-04-02 18:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,40 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+const errorDiv = document.getElementById("error");
+const loading = document.getElementById("loading");
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(`Failed to load image: ${url}`);
+  });
+}
+
+function downloadImages() {
+  output.innerHTML = "";
+  errorDiv.textContent = "";
+  loading.style.display = "block";
+
+  const promises = images.map(url => downloadImage(url));
+
+  Promise.all(promises)
+    .then(imgs => {
+      loading.style.display = "none";
+
+      imgs.forEach(img => {
+        output.appendChild(img);
+      });
+    })
+    .catch(err => {
+      loading.style.display = "none";
+
+      errorDiv.textContent = err;
+    });
+}
+
+downloadImages();
